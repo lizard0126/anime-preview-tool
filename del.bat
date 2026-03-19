213b8edebd28a846bbff52e04e0d4d902e709d9f
lizard0126 <825456471@qq.com>
2026-03-19 20:14:19 +0800

    史诗级大更新之打包exe了

--- del.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del /q ".\temp\*.html"
-del /q ".\temp\assets\visual\*.*"
-del /q ".\screenshots\*.*"
-del /q ".\code\data.json"
-exit
--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-node code/copy-avatars.js
-node code/parseAnimeDocx.js
-node code/generate-html.js
-node code/screenshot-html.js
-node code/del-avatars.js
-exit
